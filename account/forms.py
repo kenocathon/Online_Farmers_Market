3c913d5053172b1efdac539cd23a4add8ab5fd8a
Keenan Ray <keenan.ray87@gmail.com>
2020-04-27 16:45:53 -0400

    added login form, and form view

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-class LoginForm(froms.Form):
+
+class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
-    
