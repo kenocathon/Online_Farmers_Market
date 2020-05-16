e129f1b1de3e814d46451093d16aa32205c59f97
Keenan Ray <keenan.ray87@gmail.com>
2020-05-16 19:30:55 -0400

    updated registration form with bootstrap theme

--- account/forms.py
+++ account/forms.py
@@ -4,15 +4,23 @@ from .models import FarmerProfile
 
 
 class UserRegistrationForm(forms.ModelForm):
-
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password = forms.CharField(
+        label='password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     password2 = forms.CharField(
-        label='Repeat password', widget=forms.PasswordInput)
+        label='Repeat password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
         fields = ('username', 'first_name',
                   'last_name', 'email',)
+
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+
+        }
 
     def clean_password2(self):
         cd = self.cleaned_data
@@ -25,6 +33,12 @@ class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email')
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+
+        }
 
 
 class FarmerProfileEditForm(forms.ModelForm):
@@ -32,3 +46,15 @@ class FarmerProfileEditForm(forms.ModelForm):
         model = FarmerProfile
         fields = ('street', 'city', 'state', 'zipcode', 'farm_name',
                   'farm_description', 'farm_products', 'farm_photo', 'status')
+        widgets = {
+            'street': forms.TextInput(attrs={'class': 'form-control'}),
+            'city': forms.TextInput(attrs={'class': 'form-control'}),
+            'state': forms.TextInput(attrs={'class': 'form-control'}),
+            'zipcode': forms.TextInput(attrs={'class': 'form-control'}),
+            'farm_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'farm_description': forms.Textarea(attrs={'class': 'form-control'}),
+            'farm_products': forms.Textarea(attrs={'class': 'form-control'}),
+            'farm_photo': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'status': forms.Select(attrs={'class': 'form-control'}),
+
+        }
--- account/views.py
+++ account/views.py
@@ -48,25 +48,4 @@ def register(request):
             return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
-    return render(request, 'account/register.html', {'user_form': user_form})
-
-# def user_login(request):
-#     if request.method == 'POST':
-#         form = LoginForm(request.POST)
-#         if form.is_valid():
-#             cd = form.cleaned_data
-#             user = authenticate(
-#                 request, username=cd['username'], password=cd['password'])
-#             if user is not None:
-#                 if user.is_active:
-#                     login(request, user)
-#                 return HttpResponse('Authenticated successfully')
-#             else:
-#                 return HttpResponse('Disabled account')
-#         else:
-#             return HttpResponse('Invalid login')
-#     else:
-#         form = LoginForm()
-#     return render(request, 'account/login.html', {'form': form})
-
-# Create your views here.
+    return render(request, 'account/register.html', {'user_form': user_form, 'section': 'register'})
