33152cea40407a5638666fc3a43e03cab228b633
Keenan Ray <keenan.ray87@gmail.com>
2020-05-20 18:07:05 -0400

    updated the settings.py for heroku

--- farmers_market/settings.py
+++ farmers_market/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*j7h7c$^3zf81o6j%#g+5m1_caigr7b=ica#!kwek*aafe0ke9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://farmers-market-app.herokuapp.com/']
 
